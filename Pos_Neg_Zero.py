2320dfc19bffe1446bf8dcce80ad44f78abf1a8a
R.Kalai Maran <kalaimaranr18@gmail.com>
2025-08-26 23:20:15 +0530

    Add files via upload

---
+++ Pos_Neg_Zero.py
@@ -0,0 +1,15 @@
+'''
+A number N is passed as the input to the program. If the number is greater than zero, the program must print positive. 
+If the number is less than zero, the program must print negative.
+If N is equal to zero, the program must print zero.
+
+'''
+#Your code below 1
+
+n=int(input())
+if n>0:
+    print("positive")
+elif n<0:
+    print("negative")
+else:
+    print("zero")
