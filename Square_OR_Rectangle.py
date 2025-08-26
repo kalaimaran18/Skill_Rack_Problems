5eb7b1d9ad3b7ea13518d953953a443fc8d0fa11
R.Kalai Maran <kalaimaranr18@gmail.com>
2025-08-26 23:39:04 +0530

    Add files via upload

---
+++ Square_OR_Rectangle.py
@@ -0,0 +1,12 @@
+'''
+The length L and breadth B of a rectangle is passed as input. If L is equal to B, the program must print SQUARE. 
+Else the program must print RECTANGLE.
+
+'''
+# enter your code 
+l=int(input())
+b=int(input())
+if l==b:
+    print("SQUARE")
+else:
+    print("RECTANGLE")
